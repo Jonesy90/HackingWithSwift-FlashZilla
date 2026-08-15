f90b5fc14393a07a06eabd3f1a0e2bfb39a64f38
Michael Jones <michael.jones90@me.com>
2026-08-15 10:28:18 +0100

    Created a Card model and 'CardView'

---
+++ HackingWithSwift-FlashZilla/Models/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  HackingWithSwift-FlashZilla
+//
+//  Created by Michael Jones on 15/08/2026.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "What is 2 + 2?", answer: "4")
+}
--- HackingWithSwift-FlashZilla/Views/CardView.swift
+++ HackingWithSwift-FlashZilla/Views/CardView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct CardView: View {
+    let card: Card
+    
+    @State private var isShowingAnswer: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.white)
+                .shadow(radius: 10)
+            
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+                
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundStyle(.secondary)
+                }
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
     }
 }
 
 #Preview {
-    CardView()
+    CardView(card: Card.example)
 }
