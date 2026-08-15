872049d89fda6382d6204317b3e60e40a6548f3a
Michael Jones <michael.jones90@me.com>
2026-08-15 09:08:50 +0100

    Initial Commit

---
+++ HackingWithSwift-FlashZilla/HackingWithSwift_FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_FlashZillaApp.swift
+//  HackingWithSwift-FlashZilla
+//
+//  Created by Michael Jones on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
