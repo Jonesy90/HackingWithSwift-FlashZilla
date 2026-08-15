58a7188c077a6625bcc519a729ced0eaa348cb6c
Michael Jones <michael.jones90@me.com>
2026-08-15 10:27:26 +0100

    Updated the setting so the app only works in landscape

---
+++ HackingWithSwift-FlashZilla/Views/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  HackingWithSwift-FlashZilla
+//
+//  Created by Michael Jones on 15/08/2026.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ HackingWithSwift-FlashZilla/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  HackingWithSwift-FlashZilla
+//
+//  Created by Michael Jones on 15/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
