1513d8e2e98b7e1c8d2bcf9d7fe52af4f436a425
Michael Jones <michael.jones90@me.com>
2026-08-17 17:56:34 +0100

    Adds some background images, gesture controls added (rotationEffect & opacity) and removing cards from the array.

--- HackingWithSwift-FlashZilla/Views/CardView.swift
+++ HackingWithSwift-FlashZilla/Views/CardView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct CardView: View {
     let card: Card
+    var removal: (() -> Void)? = nil
     
     @State private var isShowingAnswer: Bool = false
+    /// Represents how far the card has been dragged horizontally by the user (positive = right, negative = left).
+    @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
@@ -33,6 +36,29 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        /// Creates a natural and interactive effect where the card visually tilts as the user drags it left or right.
+        .rotationEffect(.degrees(offset.width / 5.0))
+        /// Moves the card left or right  (x axis) by a specific number of points.
+        .offset(x: offset.width * 5)
+        /// Adjusts the transparency of the CardView based on how far it has been dragged horizontally.
+        .opacity(2 - Double(abs(offset.width / 50)))
+        /// Attaches a drag gesture to the CardView, enabling interactive dragging of the card.
+        .gesture(
+            /// As the user drags the card, this closure is called repeatedly with the latest drag gesture data.
+            DragGesture().onChanged { gesture in
+                /// This represents how far the user has moved from the drag gestures's starting point.
+                offset = gesture.translation
+            }
+            /// This closure is called once the user ends the drag (remove their finger from the screen).
+            .onEnded { _ in
+                /// If the drag is more than 100 points on either side, it calls a closure to remove the card from the stack. Otherwise, return the card back to its starting position.
+                if abs(offset.width) > 100 {
+                    removal?()
+                } else {
+                    offset = .zero
+                }
+            }
+        )
         .onTapGesture {
             isShowingAnswer.toggle()
         }
--- HackingWithSwift-FlashZilla/Views/ContentView.swift
+++ HackingWithSwift-FlashZilla/Views/ContentView.swift
@@ -7,9 +7,41 @@
 
 import SwiftUI
 
+/// Adds a new method to all SwiftUI views and calculates how far from the bottom this item is.
+/// Creates a visual stacking effect in SwiftUI by vertically offsetting each view based on its position in a collection.
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
 struct ContentView: View {
+    @State private var cards = Array<Card>.init(repeating: .example, count: 10)
+    
     var body: some View {
-        CardView(card: Card.example)
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
+    }
+    
+    /// Removes a card from the array (of cards) at a specific position.
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
 }
 
