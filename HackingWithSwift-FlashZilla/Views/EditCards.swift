4017931c20fc7727e36cceaee0a4b129591a91a7
Michael Jones <michael.jones90@me.com>
2026-08-23 10:15:18 +0100

    An edit view providing a section that allows the user to create new cards and another section showing a list of all the cards currently available.

--- HackingWithSwift-FlashZilla/Views/EditCards.swift
+++ HackingWithSwift-FlashZilla/Views/EditCards.swift
@@ -8,8 +8,74 @@
 import SwiftUI
 
 struct EditCards: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var cards = [Card]()
+    @State private var newPrompt = String()
+    @State private var newAnswer = String()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section("Adding New Card") {
+                    TextField("Prompt", text: $newPrompt)
+                    TextField("Answer", text: $newAnswer)
+                    Button("Add Card", action: addCard)
+                }
+                
+                Section() {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        VStack(alignment: .leading) {
+                            Text(cards[index].prompt)
+                                .font(.headline)
+                            
+                            Text(cards[index].answer)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+                    .onDelete(perform: removeCards)
+                }
+            }
+            .navigationTitle("Edit Cards")
+            .toolbar {
+                Button("Done", action: done)
+            }
+            .onAppear(perform: loadData)
+        }
+    }
+    
+    func done() {
+        dismiss()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
+    }
+    
+    func saveData() {
+        if let data = try? JSONEncoder().encode(cards) {
+            UserDefaults.standard.set(data, forKey: "Cards")
+        }
+    }
+    
+    func addCard() {
+        let trimmedPrompt = newPrompt.trimmingCharacters(in: .whitespaces)
+        let trimmedAnswer = newAnswer.trimmingCharacters(in: .whitespaces)
+        
+        guard trimmedPrompt.isEmpty == false && trimmedAnswer.isEmpty == false else { return }
+        
+        let newCard = Card(prompt: trimmedPrompt, answer: trimmedAnswer)
+        cards.insert(newCard, at: 0)
+        saveData()
+    }
+    
+    func removeCards(at offsets: IndexSet) {
+        cards.remove(atOffsets: offsets)
+        saveData()
     }
 }
 
