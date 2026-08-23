53c3d810780cccb0ea5f13172739d51e1198e3d8
Michael Jones <michael.jones90@me.com>
2026-08-23 10:11:02 +0100

    Struct updated so it conforms to Codable. This allows us to decode and encode Card objects with UserDefaults.

---
+++ HackingWithSwift-FlashZilla/Views/EditCards.swift
@@ -0,0 +1,18 @@
+//
+//  EditCards.swift
+//  HackingWithSwift-FlashZilla
+//
+//  Created by Michael Jones on 22/08/2026.
+//
+
+import SwiftUI
+
+struct EditCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCards()
+}
